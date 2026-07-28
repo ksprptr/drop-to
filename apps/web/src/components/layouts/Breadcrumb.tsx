c9e0e82a3c1669dbb35e4abcbc6b7183fe77f19c
Petr Kašpar <petr.kaspar@ksprptr.dev>
2026-07-28 16:34:31 +0200

    style(web): max width simplified

--- apps/web/src/components/layouts/Breadcrumb.tsx
+++ apps/web/src/components/layouts/Breadcrumb.tsx
@@ -97,7 +97,7 @@ export default function Breadcrumb({
               : 'text-zinc-600 dark:text-zinc-400'
           }`}>
           <Icon icon={rootIcon} className='h-4 w-4 shrink-0' />
-          <span className='max-w-[10rem] truncate'>{rootLabel}</span>
+          <span className='max-w-40 truncate'>{rootLabel}</span>
           {isPicker && <Icon icon='ChevronUpDown' className='h-3.5 w-3.5 shrink-0 opacity-60' />}
         </button>
 
